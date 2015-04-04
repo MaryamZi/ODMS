cea84f5ba611a8fc379540da19cfaf969ec9fbb8
chamil <chamilprabodha.12@cse.mrt.ac.lk>
2015-04-04 18:57:47 +0530

    Login page finished

---
+++ ODMS/Main/Mainfrm.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace ODMS.Main
+{
+    partial class Mainfrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.receptionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kitchenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.diliveryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.permissionLbl = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusLbl = new System.Windows.Forms.ToolStripStatusLabel();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem,
+            this.receptionToolStripMenuItem,
+            this.kitchenToolStripMenuItem,
+            this.diliveryToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(736, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // receptionToolStripMenuItem
+            // 
+            this.receptionToolStripMenuItem.Name = "receptionToolStripMenuItem";
+            this.receptionToolStripMenuItem.Size = new System.Drawing.Size(72, 20);
+            this.receptionToolStripMenuItem.Text = "Reception";
+            this.receptionToolStripMenuItem.Click += new System.EventHandler(this.receptionToolStripMenuItem_Click);
+            // 
+            // kitchenToolStripMenuItem
+            // 
+            this.kitchenToolStripMenuItem.Name = "kitchenToolStripMenuItem";
+            this.kitchenToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.kitchenToolStripMenuItem.Text = "Kitchen";
+            // 
+            // diliveryToolStripMenuItem
+            // 
+            this.diliveryToolStripMenuItem.Name = "diliveryToolStripMenuItem";
+            this.diliveryToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.diliveryToolStripMenuItem.Text = "Dilivery";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.permissionLbl,
+            this.statusLbl});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 24);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.statusStrip1.Size = new System.Drawing.Size(736, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // permissionLbl
+            // 
+            this.permissionLbl.Name = "permissionLbl";
+            this.permissionLbl.Size = new System.Drawing.Size(104, 17);
+            this.permissionLbl.Text = "Permission Level : ";
+            // 
+            // statusLbl
+            // 
+            this.statusLbl.Name = "statusLbl";
+            this.statusLbl.Size = new System.Drawing.Size(0, 17);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Mainfrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(736, 261);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Mainfrm";
+            this.Text = "Mainfrm";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Mainfrm_FormClosed);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem receptionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kitchenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem diliveryToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel permissionLbl;
+        private System.Windows.Forms.ToolStripStatusLabel statusLbl;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
--- ODMS/Security/Login.cs
+++ ODMS/Security/Login.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ODMS.Main;
+using ODMS.Security;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -20,6 +22,22 @@ namespace ODMS
         private void btnCancel_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            LoginValidate newvalidation = new LoginValidate(txtUsername.Text,txtPassword.Text);
+            if (newvalidation.validateLogin())
+            {
+                Mainfrm mainWin = new Mainfrm();
+                mainWin.Show();
+                this.Hide();
+            }
+
+            else
+            {
+                MessageBox.Show("Check login and retry");
+            }
         }
 
         
